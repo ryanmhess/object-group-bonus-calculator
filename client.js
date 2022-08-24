5ef15e88355a0b4f1128ca1ad76cd23aa4ea9e81
Ryan Hess <ryanmhess@gmail.com>
2022-08-23 21:39:35 -0500

    cleaned up and added spacers

--- client.js
+++ client.js
@@ -31,9 +31,23 @@ const employees = [
   }
 ];
 
+// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+
+// This problem is massive! Break the problem down, take small steps, and test as you go.
+// What is the fewest lines of code I can write and test to get just a little closer?
+
+// This is not a race. Everyone on your team should understand what is happening.
+// Ask questions when you don't.
+
+//-----------------------------------------------------------------------------------------------//
+//  Loop to cycle through each employee
+
 for(let employee of employees){
   console.log(bonusCalc(employee));
-}
+} //  end for of loop
+
+//-----------------------------------------------------------------------------------------------//
+//  Bonus Calc Function
 
 function bonusCalc(employee){
   bonusParameter1(employee);
@@ -47,54 +61,60 @@ function bonusCalc(employee){
     totalCompensation: Number(bonusPercentage * employee.annualSalary) + Number(employee.annualSalary)
   }
   return employeeBonusStuff;
-}
+} //  end bonusCalc function
+
+//-----------------------------------------------------------------------------------------------//
+//  Bonus Parameter 1 Function
 
 function bonusParameter1(employee){
   if(employee.reviewRating <= 2){
     bonusPercentage = 0;
-  }
+  } //  end if
   else if(employee.reviewRating === 3){
     bonusPercentage = .04;
-  }
+  } //  end else if
   else if(employee.reviewRating === 4){
     bonusPercentage = .06;
-  }
+  } //  end else if
   else if(employee.reviewRating === 5){
   bonusPercentage = .1;
-  }
+  } //  end else if
   return bonusPercentage;
-}
+} //  end bonusParameter1 function
+
+//-----------------------------------------------------------------------------------------------//
+//  Bonus Parameter 2 Function
 
 function bonusParameter2(employee){
   if(employee.employeeNumber.length === 4){
     bonusPercentage += .05;
-  }
+  } //  end if
   return bonusPercentage;
-}
+} //  end bonusParameter2 function
+
+//-----------------------------------------------------------------------------------------------//
+//  Bonus Parameter 3 Function
 
 function bonusParameter3(employee){
   if(employee.annualSalary > '65000'){
     bonusPercentage -= .01;
-  }
+  } //  end if
   return bonusPercentage;
-}
+} //  end bonusParameter3 function
+
+//-----------------------------------------------------------------------------------------------//
+//  Bonus Parameter 4 Function
 
 function bonusParameter4(){
   if(bonusPercentage > .13){
     bonusPercentage = .13;
-  }
+  } //  end if
   else if(bonusPercentage < 0){
     bonusPercentage = 0;
-  }
+  } //  end else if
   return bonusPercentage;
-}
+} //  end bonusParameter4 function
 
-// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+//-----------------------------------------------------------------------------------------------//
 
-// This problem is massive! Break the problem down, take small steps, and test as you go.
-// What is the fewest lines of code I can write and test to get just a little closer?
-
-// This is not a race. Everyone on your team should understand what is happening.
-// Ask questions when you don't.
-
-//console.log( employees );
+console.log( employees );
